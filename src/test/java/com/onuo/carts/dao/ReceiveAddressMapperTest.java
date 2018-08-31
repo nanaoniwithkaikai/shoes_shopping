391bba88991dbf611bc311afa0861c20c6a5a5cf
Jachken <489584815@qq.com>
2018-08-31 09:35:41 +0800

    2018/8/31 9:35

--- src/test/java/com/onuo/carts/dao/ReceiveAddressMapperTest.java
+++ src/test/java/com/onuo/carts/dao/ReceiveAddressMapperTest.java
@@ -14,6 +14,7 @@ public class ReceiveAddressMapperTest{
         System.out.println("wangyingchangceshi");
         System.out.println("殷桂根帅！！！");
         System.out.println("吴冬前更帅！！！");
+        System.out.println("姜明锐最帅!!!!!!!!");
 
 
     }
