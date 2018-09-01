998828104134aaf6fe1a46cb0ecf843e8414a6b0
wudongqian <1970422794@qq.com>
2018-09-01 10:09:12 +0800

    吴冬前 提交2018/9/1 10:08

--- src/main/java/com/onuo/carts/dao/ReceiveAddressMapper.java
+++ src/main/java/com/onuo/carts/dao/ReceiveAddressMapper.java
@@ -2,6 +2,7 @@ package com.onuo.carts.dao;
 
 import com.onuo.carts.model.ReceiveAddress;
 import com.onuo.common.mapper.MyMapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -14,25 +15,26 @@ public interface ReceiveAddressMapper extends MyMapper<ReceiveAddress> {
 
     /**
      @param addressId 要删除的收件地址ID
-     @param custId 当前用户ID
      @return 返回是否删除成功,true表示删除成功，false表示删除失败
      */
-    int delete(String addressId,String custId);
+    int delete(String addressId);
 
     /**
      @param address 要修改的收件地址对象
      @return 返回是否修改成功,true表示修改成功，false表示修改失败
      */
     int update(ReceiveAddress address);
+
     /**
      @param custId 当前用户ID
      @return 返回当前客户所有的
      */
-      List<ReceiveAddress> queryAll(String custId);
+    List<ReceiveAddress> queryAll(String custId);
+
     /**
      @param addressId 要设置为默认收件地址的地址ID
      @param custId 当前用户ID
-     @return 返回是否修改成功,true表示修改成功，false表示修改失败
+     @return 1表示默认地址
      */
-    int updateDefaultAddress(String addressId,String custId);
+    int updateDefaultAddress(@Param("addressId") String addressId, @Param("custId") String custId);
 }
--- src/test/java/com/onuo/carts/dao/ReceiveAddressMapperTest.java
+++ src/test/java/com/onuo/carts/dao/ReceiveAddressMapperTest.java
@@ -13,37 +13,31 @@ import java.util.List;
 public class ReceiveAddressMapperTest {
     ReceiveAddressMapper mapper = (ReceiveAddressMapper) CommonMapper.getInstance().getMapper(ReceiveAddressMapper.class);
 
-    @Test
-    public void testQuery() {
-        System.out.print(1);
-        List<ReceiveAddress> list = mapper.queryAll("1");
-        for (ReceiveAddress r : list)
-            System.out.println(r);
-        System.out.println("wangyingchangceshi");
-        System.out.println("殷桂根帅！！！");
-        System.out.println("吴冬前更帅！！！");
-    }
+
 
     @Test
     public void savetest() {
         ReceiveAddress address = new ReceiveAddress();
         address.setAddressId(IdGenerator.getNewUserId());
-        address.setAddressProvince("小安徽");
-        address.setAddressCity("六安");
-        address.setAddressStreet("红街");
-        address.setAddressDetail("皖西学院");
-        address.setAddressPostCode(null);
-        address.setAddressRecipientName(null);
+        address.setAddressProvince("江苏");
+        address.setAddressCity("南京");
+        address.setAddressArea("江宁区");
+        address.setAddressStreet("步行街");
+        address.setAddressDetail("夫子庙");
+        address.setAddressPostCode(250);
+        address.setAddressRecipientName("隔壁老王");
         address.setAddressRecipientTelno(18860423212L);
-        address.setIsDefaultAddress(null);
-        address.setCustId("1001");
-        mapper.save(address);
+        address.setIsDefaultAddress(0);
+        address.setCustId("18083176086190");
+        int rows=mapper.save(address);
+        System.out.println("返回值:"+rows);
         MybatisUtil.closeSession();
     }
 
     @Test
     public void testupdate() {
         ReceiveAddress address = new ReceiveAddress();
+        address.setAddressId("18090169093696");
         address.setAddressProvince("安徽1222");
         address.setAddressCity("六安");
         address.setAddressArea("裕安区");
@@ -53,9 +47,30 @@ public class ReceiveAddressMapperTest {
         address.setAddressRecipientName(null);
         address.setAddressRecipientTelno(18860423212L);
         address.setIsDefaultAddress(null);
-        address.setCustId("1001");
+        address.setCustId("18083176086190");
         mapper.update(address);
         MybatisUtil.closeSession();
+    }
+
+    @Test
+    public void testUpdateDefaultAddress(){
+        mapper.updateDefaultAddress("18090169093696","18083176086190");
+        MybatisUtil.closeSession();
+
+    }
+
+    @Test
+    public void testDelete(){
+        mapper.delete("18090191745341");
+        MybatisUtil.closeSession();
+
+    }
+
+    @Test
+    public void testQueryAll() {
+        List<ReceiveAddress> list = mapper.queryAll("18083176086190");
+        for (ReceiveAddress r : list)
+            System.out.println(r);
     }
 }
 
